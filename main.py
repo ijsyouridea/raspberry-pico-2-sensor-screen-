74318d6cb873088f2130ad50623d858d86c344b6
Валерій Зінченко <zinchenkomac@icloud.com>
2026-08-19 20:12:50 +0200

    feat: added color diod to sygnalise read write to memory

--- main.py
+++ main.py
@@ -6,29 +6,58 @@ from machine import Pin
 from lcd1602 import LCD
 
 lcd = LCD()
+counter_file = "counter.txt"
+
+
+def read_counter():
+    try:
+        with open(counter_file, "r") as file:
+            return int(file.read().strip() or "0")
+    except (OSError, ValueError):
+        return 0
+
+
+def write_counter(counter):
+    with open(counter_file, "w") as file:
+        file.write(str(counter))
+
 
 # Initialize DHT11 on GPIO 16 (Pin 21)
 sensor = DHT11(Pin("GP16", Pin.OUT))
 green = Pin("GP0", Pin.OUT)
 red = Pin("GP1", Pin.OUT)
+yellow = Pin("GP12", Pin.OUT)
 # Allow sensor to boot up
 time.sleep(2)
 
 
 while True:
     try:
-        lcd.clear()
+        yellow.on()
+        counter = read_counter()
+        yellow.off()
+
+        counter += 1
+
         green.on()
         sensor.measure()
         temp = sensor.temperature()
         hum = sensor.humidity()
         green.off()
+
+        yellow.on()
+        write_counter(counter)
+        yellow.off()
+
         red.on()
         # print(f"Temperature: {temp}°C   Humidity: {hum}%")
-        lcd.write(0, 0, f"Temp: {temp}C")
-        lcd.write(0, 1, f"Hum: {hum}%")
+        lcd.clear()
+        lcd.write(0, 0, f"Count: {counter}")
+        lcd.write(0, 1, f"Temp:{temp}C Hum:{hum}%")
         red.off()
+
     except OSError as e:
+        yellow.off()
         print("Failed to read sensor.", e)
 
     time.sleep(2)
